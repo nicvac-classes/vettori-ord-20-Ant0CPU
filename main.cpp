0d9f6c9d7778aa9850e7d5059a4c8fd0f5c21cb0
Ant0CPU <antonio.bruno@marconibari.edu.it>
2025-05-21 11:05:10 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,65 @@
 #include <iostream>
+#include <cmath>
+#include <ctime>
+#include <vector>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    srand(time(0));
+
+    int n, i, j, t, k, c, r;
+    bool scambi(true);
+
+    do {
+        cout<<"Quanti numeri generare?"<<endl;
+        cin>>n;
+    } while (n<=0);
+
+    vector<int>l(n);
+    for (i=0; i<n; i++) {
+        l[i]= 4+(rand() % 7);
+    }
+
+    i=0;
+    while (i<n && scambi==true) {
+        scambi=false;
+        j=0;
+
+        while (j<(n-1)-i) {
+            if (l[j]>l[j+1]) {
+
+                t=l[j];
+                l[j]=l[j+1];
+                l[j+1]=t;
+
+                scambi=true;
+            }
+            j=j+1;
+        }
+        i=i+1;
+    }
+    
+    cout<<"Lunghezze dei listelli: ";
+    for (i=0; i<n; i++) {
+        cout<<l[i]<<"; ";
+    }
+
+    c=0;
+    if (n>4) {
+        for (i=0; i<=n-2; i++) {
+            k=i+1;
+
+            if (l[i]=l[k]) {
+                c=c+1;;
+            }
+        }
+
+        r=c/2;
+        cout<<"Rettangoli: "<<r<<endl;
+    }
+    else
+    {
+        cout<<"Non puoi creare rettangoli"<<endl;
+    }
+}
